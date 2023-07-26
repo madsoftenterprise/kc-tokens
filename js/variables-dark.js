539a6b0feaba49b4495dc743180f93ca4194085e
fEkcL <miha.shkolnikov@gmail.com>
2023-07-26 17:42:33 +0300

    First commit

---
+++ js/variables-dark.js
@@ -0,0 +1,57 @@
+/**
+ * Do not edit directly
+ * Generated on Mon, 12 Dec 2022 11:27:33 GMT
+ */
+
+export const InteractiveLight = "#e6eeff";
+export const InteractiveLightHover = "#b4cdff";
+export const InteractiveDefault = "#689aff";
+export const InteractiveHover = "#457ef0";
+export const GrayUnchangeable0 = "#fcfcfc";
+export const GrayUnchangeable900 = "#06080e";
+export const ValidationSuccessMain = "#45f067";
+export const ValidationSuccessBg = "#e7faeb";
+export const ValidationSuccessContent = "#0d6b23";
+export const ValidationWarningMain = "#f2bd04";
+export const ValidationWarningBg = "#fff8cb";
+export const ValidationWarningContent = "#673c09";
+export const ValidationErrorMain = "#f04b45";
+export const ValidationErrorBg = "#fbedef";
+export const ValidationErrorContent = "#951a20";
+export const HeatStatusBlue = "#94aaaf";
+export const HeatStatusBlueAlpha = "#b4ceff70";
+export const HeatStatusGreen = "#45f067";
+export const HeatStatusGreenAlpha = "#45f06770";
+export const HeatStatusYellow = "#f0bc03";
+export const HeatStatusYellowAlpha = "#f0bc0370";
+export const HeatStatusRed = "#f04b45";
+export const HeatStatusRedAlpha = "#f04b4570";
+export const MacroRegionPink = "#ee2d62";
+export const MacroRegionPinkAlpha = "#ee2d6299";
+export const MacroRegionOrange = "#f8931f";
+export const MacroRegionOrangeAlpha = "#f8931f99";
+export const MacroRegionDarkGreen = "#45a040";
+export const MacroRegionDarkGreenAlpha = "#45a04099";
+export const MacroRegionBlue = "#4ec6df";
+export const MacroRegionBlueAlpha = "#4ec6df99";
+export const MacroRegionOutlinePink = "#f986a5";
+export const MacroRegionOutlineOrange = "#f6ce9e";
+export const MacroRegionOutlineDarkGreen = "#54ed76";
+export const MacroRegionOutlineBlue = "#b8ebf5";
+export const ScorePerfectly = "#45f06b";
+export const ScoreGood = "#b9f045";
+export const ScoreSatisfactory = "#f0ca45";
+export const ScoreBadly = "#f08345";
+export const ScoreVeryBad = "#f04545";
+export const ShadowDefaultX = "2px";
+export const ShadowDefaultY = "2px";
+export const ShadowDefaultBlur = "12px";
+export const ShadowDefaultSpread = "0px";
+export const ShadowDefaultColor = "#0512460f";
+export const ShadowDefaultType = "dropShadow";
+export const ShadowActiveX = "2px";
+export const ShadowActiveY = "2px";
+export const ShadowActiveBlur = "12px";
+export const ShadowActiveSpread = "0px";
+export const ShadowActiveColor = "#0208201f";
+export const ShadowActiveType = "dropShadow";
